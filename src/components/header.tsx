2136c341ec309144776034d4a4a8a84739c92552
CMBVT2023 <cristianomirandabvt2023@gmail.com>
2025-02-26 17:39:19 -0800

    Reformatted the page

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,7 +5,7 @@ import { redirect } from "next/navigation";
 interface HeaderProps {
   showLoginButton: boolean;
   showRegisterButton: boolean;
-  isUserLoggedIn: boolean
+  isUserLoggedIn: boolean;
   children: React.ReactNode;
 }
 
@@ -13,7 +13,7 @@ export default function Header({
   showLoginButton,
   showRegisterButton,
   isUserLoggedIn,
-  children
+  children,
 }: HeaderProps) {
   function redirectLogin() {
     redirect("/user/login");
@@ -26,31 +26,43 @@ export default function Header({
   async function signUserOut() {
     const isUserSignedOut = deleteUserLogin();
     if (!isUserSignedOut) {
-      alert('Signout failed!')
+      alert("Signout failed!");
       return;
-    };
+    }
     redirect("/user/login");
   }
 
   const RenderSignInOrOut = () => {
-    return isUserLoggedIn ? (<button onClick={signUserOut} className="h-full outline outline-white p-1">
-      Sign Out
-    </button>
-  ):
-   (<button onClick={redirectLogin} className="h-full outline outline-white p-1">
-      Sign In
-    </button>
-  )
-  }
+    return isUserLoggedIn ? (
+      <button
+        onClick={signUserOut}
+        className="h-full outline outline-white p-1"
+      >
+        Sign Out
+      </button>
+    ) : (
+      <button
+        onClick={redirectLogin}
+        className="h-full outline outline-white p-1"
+      >
+        Sign In
+      </button>
+    );
+  };
 
   return (
     <>
       <div className="h-12 w-full p-2 flex justify-between items-center bg-blue-800 text-white">
-        <Link href={{pathname: "/"}} className="text-xl">Financial Tracker</Link>
+        <Link href={{ pathname: "/" }} className="text-xl">
+          Financial Tracker
+        </Link>
         <div className="h-full flex gap-2">
           {showLoginButton && <RenderSignInOrOut />}
           {showRegisterButton && (
-            <button onClick={redirectRegistration} className="h-full outline outline-white p-1">
+            <button
+              onClick={redirectRegistration}
+              className="h-full outline outline-white p-1"
+            >
               Sign Up
             </button>
           )}
